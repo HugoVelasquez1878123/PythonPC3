17a0316982ed2065593279c83048017cda15024a
HugoVelasquez1878123 <145520403+HugoVelasquez1878123@users.noreply.github.com>
2023-10-08 16:13:05 +0000

    Ejercicio 2 - PC3

---
+++ PC3_Ejercicio2.py
@@ -0,0 +1,18 @@
+def funcion_numeros():
+    while True:
+        entrada = input("Ingrese números enteros o con decimales separados por comas: ")
+        
+        try:
+            numeros_texto = entrada.split(',') 
+            numeros_decimales = [float(num.strip()) for num in numeros_texto]
+            numeros_enteros = [int(num) for num in numeros_decimales]
+            return numeros_enteros
+        except ValueError:
+            print("Error: Hubo un problema al convertir los números. "
+                  "Por favor, asegúrese de ingresar números válidos.")
+
+if __name__ == '__main__':
+    numeros_enteros = funcion_numeros()
+    
+    if numeros_enteros:
+        print("Números enteros obtenidos:", numeros_enteros)
